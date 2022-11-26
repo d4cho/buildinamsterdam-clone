a9c18c0849ffb7505c482c8e68cdc9d3887468c5
bioDanCho <daniel.cho@thebioagency.com>
2022-11-26 16:19:39 -0500

    nav item routing

--- assets/data/nav-data.js
+++ assets/data/nav-data.js
@@ -3,25 +3,30 @@ export const navData = [
         title: 'home',
         imgUrl: '/images/nav-home.webp',
         mobileImgUrl: '/images/mobile-nav-home.webp',
+        linkTo: '/',
     },
     {
         title: 'work',
         imgUrl: '/images/nav-work.webp',
         mobileImgUrl: '/images/mobile-nav-work.webp',
+        linkTo: '/cases',
     },
     {
         title: 'about',
         imgUrl: '/images/nav-about.webp',
         mobileImgUrl: '/images/mobile-nav-about.webp',
+        linkTo: '/about',
     },
     {
         title: 'contact',
         imgUrl: '/images/nav-contact.webp',
         mobileImgUrl: '/images/mobile-nav-contact.webp',
+        linkTo: '/contact',
     },
     {
         title: 'join us',
         imgUrl: '/images/nav-join.webp',
         mobileImgUrl: '/images/mobile-nav-join.webp',
+        linkTo: '/join',
     },
 ];
--- components/NavItem.js
+++ components/NavItem.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { useAppContext } from '../context/AppContext';
 import styles from '../styles/NavItem.module.css';
+import Link from 'next/link';
 
 const NavItem = ({ data }) => {
-    const { view } = useAppContext();
-    const { title, imgUrl, mobileImgUrl } = data;
+    const { view, setIsMenuOpen } = useAppContext();
+    const { title, imgUrl, mobileImgUrl, linkTo } = data;
 
     return (
-        <div className={styles.container}>
-            <div className={styles.dot} />
-            <div className={styles.title}>{title}</div>
-            <div className={styles.image_container}>
-                <img
-                    className={styles.image}
-                    src={view === 'mobile' ? mobileImgUrl : imgUrl}
-                    alt={title}
-                />
-            </div>
+        <div className={styles.container} onClick={() => setIsMenuOpen(false)}>
+            <Link href={linkTo}>
+                <div className={styles.dot} />
+                <div className={styles.title}>{title}</div>
+                <div className={styles.image_container}>
+                    <img
+                        className={styles.image}
+                        src={view === 'mobile' ? mobileImgUrl : imgUrl}
+                        alt={title}
+                    />
+                </div>
+            </Link>
         </div>
     );
 };

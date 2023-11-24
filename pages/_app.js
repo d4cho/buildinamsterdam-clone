c0b685238d38b409171b3494a421b2afc3eec03c
d4cho <danielcho91@gmail.com>
2023-11-24 09:25:03 -0500

    bio widget added

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
-import '../styles/globals.css';
-import Layout from '../components/Layout';
-import { AppContextProvider } from '../context/AppContext';
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import { AppContextProvider } from "../context/AppContext";
+import BioStorybookWidget from "bio-storybook-widget";
 
 const MyApp = ({ Component, pageProps }) => {
-    return (
-        <AppContextProvider>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </AppContextProvider>
-    );
+  return (
+    <AppContextProvider>
+      <BioStorybookWidget projectSource={"bia"} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppContextProvider>
+  );
 };
 
 export default MyApp;
